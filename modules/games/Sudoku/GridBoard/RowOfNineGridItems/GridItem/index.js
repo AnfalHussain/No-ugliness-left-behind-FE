5241a2f3ce77568f3caff2a090361ceda71e70ac
AnfalHussain <anfalae95@gmail.com>
2021-05-05 02:06:23 +0300

    style - handle input change

--- modules/games/Sudoku/GridBoard/RowOfNineGridItems/GridItem/index.js
+++ modules/games/Sudoku/GridBoard/RowOfNineGridItems/GridItem/index.js
@@ -1,10 +1,40 @@
-const GridItem = ({ cellValue }) => {
-  // item should have a state to track it's current value and it's correct value
+import { useState } from "react";
+
+const GridItem = ({ cellValue, rowIndex, columnIndex }) => {
+  // item should have a state to track its current value and its correct value
+
+  const [inputValue, setInputValue] = useState(cellValue);
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    const lastCharacter = value.slice(-1);
+
+    const isNumber = Number.isInteger(Number(lastCharacter));
+    const isEmptyString = value === "";
+
+    if ((isNumber && lastCharacter > 0) || isEmptyString)
+      setInputValue(lastCharacter);
+  };
+
+  const cellBackgroundColor =
+    (rowIndex < 3 && columnIndex < 3) ||
+    (rowIndex < 3 && columnIndex > 5) ||
+    (rowIndex > 2 && rowIndex < 6 && columnIndex > 2 && columnIndex < 6) ||
+    (rowIndex > 5 && columnIndex < 3) ||
+    (rowIndex > 5 && columnIndex > 5)
+      ? "bg-gray-900 bg-opacity-75"
+      : "bg-blue-900 bg-opacity-75";
+
+  const cellText = !!cellValue
+    ? "text-white font-bold"
+    : "text-gray-300 italic";
   return (
     <input
-      className="box-border h-16 w-16 p-4 border-2"
+      className={`text-white box-border h-16 w-16 p-4 border-2 border-black text-center ${cellBackgroundColor} ${cellText}`}
       type="text"
-      value={cellValue}
+      value={inputValue}
+      onChange={handleInputChange}
+      disabled={!!cellValue}
     />
   );
 };
--- modules/games/Sudoku/GridBoard/RowOfNineGridItems/index.js
+++ modules/games/Sudoku/GridBoard/RowOfNineGridItems/index.js
@@ -2,7 +2,12 @@ import GridItem from "./GridItem";
 
 const RowOfNineGridItems = ({ rowValues, rowIndex }) => {
   const rowOfItems = rowValues.map((cellValue, columnIndex) => (
-    <GridItem key={`cell[${rowIndex}][${columnIndex}]`} cellValue={cellValue} />
+    <GridItem
+      key={`cell[${rowIndex}][${columnIndex}]`}
+      rowIndex={rowIndex}
+      columnIndex={columnIndex}
+      cellValue={cellValue}
+    />
   ));
 
   return <div className="flex justify-center">{rowOfItems}</div>;
